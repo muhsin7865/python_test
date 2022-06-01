ffba0a113ad0f3c08a1d4fa98d363cb09b7f161c
muhsin7865 <76041014+muhsin7865@users.noreply.github.com>
2022-06-01 08:41:46 +0400

    Update solutions.py

--- solutions.py
+++ solutions.py
@@ -1,1 +1,10 @@
 # Use this file to implement your solutions
+def biggest(array):
+    
+    greatest = array[0]
+    for i in array:
+        if(i > greatest):
+            greatest = i
+
+    return greatest
+    
