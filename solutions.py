37dbfd67dc82f0247e66a690433c0f8ab917720f
muhsin7865 <76041014+muhsin7865@users.noreply.github.com>
2022-06-01 12:48:58 +0400

    Update solutions.py

--- solutions.py
+++ solutions.py
@@ -1,10 +1,19 @@
 # Use this file to implement your solutions
-def biggest(array):
+def biggest(n):
     
-    greatest = array[0]
-    for i in array:
+    greatest = n[0]
+    for i in n:
         if(i > greatest):
             greatest = i
 
     return greatest
     
+def biggest2(n):
+    for i in range(len(n)):
+       for j in range(len(n)):
+            if(n[i]<n[j]):
+               temp = n[i]
+               n[i] = n[j]
+               n[j] = temp
+
+    return n[len(n)-2]
