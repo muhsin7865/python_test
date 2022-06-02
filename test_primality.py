4dbb35c9703fdc517161204d20e16d184e342265
muhsin7865 <76041014+muhsin7865@users.noreply.github.com>
2022-06-02 12:16:47 +0400

    Update test_primality.py

--- test_primality.py
+++ test_primality.py
@@ -1,1 +1,11 @@
 # Write the tests for the isprime.py file here
+import math
+ 
+def isprime(n):
+    if n%2 == 0 or n%3 == 0 or n%5==0:
+        return True
+    for i in range(7, n):
+        if n%i == 0:
+            return False
+            break
+    return True
